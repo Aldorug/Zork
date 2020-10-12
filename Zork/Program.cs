f03535cd3f64c326467feadadbb3ccd8c40694d2
Aldorug <54644502+Aldorug@users.noreply.github.com>
2020-10-12 17:03:27 -0400

    Zork 4.3 - A Larger World

---
+++ Zork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Zork
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
+        }
+
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
+    }  
+}
